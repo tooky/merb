66791292f84b1831001dca9e1256ea4a2a697dbc
Brennan Dunn <me@brennandunn.com>
2008-11-09 22:46:04 -0500

    Fixed misspelled instance var in generated controller tests (test_unit)
    Signed-off-by: Michael S. Klishin <michael@novemberain.com>

--- merb-gen/lib/generators/templates/component/controller/test/requests/%file_name%_test.rb
+++ merb-gen/lib/generators/templates/component/controller/test/requests/%file_name%_test.rb
@@ -6,7 +6,7 @@ class <%= full_class_name %>; def rescue_action(e) raise e end; end
 class <%= full_class_name %>Test < Test::Unit::TestCase
 
   def setup
-    @resonse = request("/<%= full_class_name.to_s.to_const_path %>")
+    @response = request("/<%= full_class_name.to_s.to_const_path %>")
   end
 
   # Replace this with your real tests.
