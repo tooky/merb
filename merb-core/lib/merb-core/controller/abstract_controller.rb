c610fc24eba7c17e0179de9e126e0368acbab58b
Matt Aimonetti <mattaimonetti@gmail.com>
2008-11-26 19:53:36 -0500

    edited the syntax to be Ruby 1.9 compatible

--- merb-core/lib/merb-core/controller/abstract_controller.rb
+++ merb-core/lib/merb-core/controller/abstract_controller.rb
@@ -417,8 +417,10 @@ class Merb::AbstractController
   # :api: private
   def _evaluate_condition(condition)
     case condition
-    when Symbol : self.send(condition)
-    when Proc : self.instance_eval(&condition)
+    when Symbol
+      self.send(condition)
+    when Proc
+      self.instance_eval(&condition)
     else
       raise ArgumentError,
             'Filter condtions need to be either a Symbol or a Proc'
