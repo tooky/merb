8b19769a9de269f79f65855a8bece6d4fcfd135b
Daniel Neighman <has.sox@gmail.com>
2008-12-01 22:05:48 +1100

    Updates the version number for the merb-auth-slice-password according to http://merb.lighthouseapp.com/projects/7433/tickets/1029

--- merb-auth/merb-auth-slice-password/lib/merb-auth-slice-password.rb
+++ merb-auth/merb-auth-slice-password/lib/merb-auth-slice-password.rb
@@ -25,7 +25,7 @@ if defined?(Merb::Plugins)
     
     # Slice metadata
     self.description = "MerbAuthSlicePassword is a merb slice that provides basic password based logins"
-    self.version = "0.9.10"
+    self.version = Gem.loaded_specs["merb-core"].version.version rescue Merb::VERSION
     self.author = "Daniel Neighman"
     
     # Stub classes loaded hook - runs before LoadClasses BootLoader
